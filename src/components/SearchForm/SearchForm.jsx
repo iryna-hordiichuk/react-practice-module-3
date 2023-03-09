5f02879319f36fb916c9830f13ced87956cfd370
Iryna Hordiichuk <72663259+IrynaHordiichuk@users.noreply.github.com>
2023-03-09 22:01:51 +0000

    task one done

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,32 @@ import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
-  render() {
-    return <h2>SearchForm</h2>;
-  }
+
+  state = {
+    query: '',
+  };
+
+handleInput = (event) => {
+const query = event.target.value;
+this.setState({query: query});
+
+
 }
+
+handleSubmit = (event) => {
+  event.preventDefault();
+  this.props.handleForm(this.state.query);
+  this.setState({query: ''});
+}
+
+  render() {
+    return <>
+<SearchFormStyled onSubmit={this.handleSubmit}>
+  <InputSearch onChange={this.handleInput}/>
+  <FormBtn>
+    <FiSearch/>
+  </FormBtn>
+</SearchFormStyled>
+    </>
+  };
+};
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -4,10 +4,80 @@ import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
+  state = {
+    query: '',
+    page: 1,
+    images: [],
+    isLoading: false,
+    isShown: false,
+    isEmpty: false,
+  };
+
+  componentDidUpdate(_, prevState) {
+    const { query, page } = this.state;
+    if (prevState.query !== query || prevState.page !== page) {
+      this.getImagesGallery(query, page);
+    }
+  }
+
+  handleForm = query => {
+    this.setState({
+      query: query,
+      page: 1,
+      images: [],
+      isShown: false,
+      isEmpty: false,
+    });
+  };
+
+  getImagesGallery = async (query, page) => {
+    if (!query) {
+      return;
+    }
+    this.setState({ isLoading: true });
+    try {
+      const images = await ImageService.getImages(query, page);
+      if (images.photos.length === 0) {
+        this.setState({ isEmpty: true });
+      }
+      this.setState(prevState => ({
+        images: [...prevState.images, ...images.photos],
+        isShown: prevState.page < Math.ceil(images.total_results / 15),
+      }));
+      console.log(Math.ceil(images.total_results / 15));
+    } catch (error) {
+      console.log(error);
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
+
+  getPage = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
   render() {
+    const { images } = this.state;
     return (
       <>
-        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        <SearchForm handleForm={this.handleForm} />
+        <Grid>
+          {images.map(item => (
+            <GridItem key={item.id}>
+              <CardItem color={item.avg_color}>
+                <img src={item.src.large} alt={item.alt} />
+              </CardItem>
+            </GridItem>
+          ))}
+        </Grid>
+        {this.state.isShown && (
+          <Button onClick={this.getPage}>
+            {this.state.isLoading ? 'Loading...' : 'Load More'}
+          </Button>
+        )}
+        {this.state.isEmpty && (
+          <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        )}
       </>
     );
   }
